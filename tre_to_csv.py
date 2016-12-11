9153333bffb57dfad0ed5af4b7868686f00bf58d
Melanie Melendrez <mmelendrez@gmail.com>
2016-12-11 18:53:26 +0000

    updated script

--- tre_to_csv.py
+++ tre_to_csv.py
@@ -1,11 +1,22 @@
 #!/user/bin/env python
 
+import argparse
+
 def parse_args():
-    pass
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "treefile",
+    help = ".nwk or .tre file path"
+  )
+  return parser.parse_args()
 
 def main(args):
-    pass
+  fh = open(args.treefile)
+  print_first_line(fh)
+
+def print_first_line(fh):
+  print fh.readline()
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+  args = parse_args()
+  main(args)
